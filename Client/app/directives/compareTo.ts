a08bfb482b5b3b6940224c795b965523cd2f0f20
Ivan Zakharov <ibzakharov@gmail.com>
2016-05-21 03:07:45 +0500

    compareTo directive has been added

---
+++ Client/app/directives/compareTo.ts
@@ -0,0 +1,25 @@
+﻿var compareTo = function () {
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=compareTo"
+        },
+        link: function (scope, element, attributes, ngModel) {
+            console.log(element);
+            console.log(scope);
+            console.log(attributes);
+            console.log(ngModel);
+
+
+            ngModel.$validators.compareTo = function (modelValue) {
+                return modelValue === scope.otherModelValue;
+            };
+
+            scope.$watch("otherModelValue", function () {
+                ngModel.$validate();
+            });
+        }
+    };
+};
+
+angular.module($app.name).directive("compareTo", compareTo);
